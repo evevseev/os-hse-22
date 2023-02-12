103fe78598295de07377a9a58e42730c1e0a23a4
Efmprof <evseev-2003@ya.ru>
2023-02-12 17:06:30 +0000

    [Sem-4]: Make file_copy return exit code

--- seminar-4/c-source/main.c
+++ seminar-4/c-source/main.c
@@ -16,7 +16,7 @@
 
 #define BUFFER_SIZE 1024 // buffer size for cycle reading
 
-void file_copy(const char *input_file, const char *output_file);
+int file_copy(const char *input_file, const char *output_file);
 
 // Main function
 int main(int argc, char *argv[]) {
@@ -27,14 +27,12 @@ int main(int argc, char *argv[]) {
     }
 
     // Call function to copy file
-    file_copy(argv[1], argv[2]);
-
-    return 0;
+    return file_copy(argv[1], argv[2]);
 }
 
 // input_file - file to copy
 // output_file - file to copy to
-void file_copy(const char *input_file, const char *output_file) {
+int file_copy(const char *input_file, const char *output_file) {
     int input_fd, output_fd; // file descriptors
     char buffer[BUFFER_SIZE]; // buffer
     ssize_t read_bytes, wrote_bytes; // bytes read and written (ssize_t is like size_t, but can be -1. we need it for error checking)
@@ -43,13 +41,13 @@ void file_copy(const char *input_file, const char *output_file) {
     // Open input_file for reading
     if ((input_fd = open(input_file, O_RDONLY)) == -1) { // O_RDONLY - open for reading only
         perror("open");
-        return;
+        return 1;
     }
 
     // Open output_file for writing
     if ((output_fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0666)) == -1) { // O_WRONLY - open for writing only, O_CREAT - create file if it doesn't exist, O_TRUNC - truncate file to 0 length if it exists
         perror("open");
-        return;
+        return 1;
     }
 
 
@@ -59,36 +57,39 @@ void file_copy(const char *input_file, const char *output_file) {
         if ((wrote_bytes = write(output_fd, buffer, read_bytes)) != read_bytes) {
             // If less bytes were written than read, then there was an error
             perror("write");
-            return;
+            return 1;
         }
     }
 
     // Check if there was an error while reading last chunk
     if (read_bytes == -1) {
         perror("read");
-        return;
+        return 1;
     }
 
     // Get input_file info
     if (fstat(input_fd, &file_stat) == -1) { 
         perror("fstat");
-        return;
+        return 1;
     } else {
         // Set output_file permissions to the same as input_file
         if (chmod(output_file, file_stat.st_mode) == -1) {
             perror("chmod");
-            return;
+            return 1;
         }
     }
 
     // Close file descriptors
     if (close(input_fd) == -1) {
         perror("close");
+        close(output_fd);
+        return 1;
     }
 
     if (close(output_fd) == -1) {
         perror("close");
+        return 1;
     }
 
-    return;
+    return 0;
 }
